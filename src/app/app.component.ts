750c8c2414b78d5f63c1d91d1d7cc25053cb1de8
alt0828 <alt0828alt0828@gmail.com>
2024-01-05 15:13:58 +0800

    標題列元件建立與實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
