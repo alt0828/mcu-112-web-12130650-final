93460a42569cd565f46558d992a7c9990ee28cf9
alt0828 <alt0828alt0828@gmail.com>
2024-01-05 15:40:03 +0800

    頁尾列元件建立與實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent],
+  imports: [HeaderComponent, NavBarComponent, FooterComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
