cd433fa5312ad7a1bd5a037b0e066dcd529bb5d6
alt0828 <alt0828alt0828@gmail.com>
2024-01-07 19:35:23 +0800

    (feat):更正登入、註冊、新文章頁面內距，並將body從中刪除

--- src/app/sign-in-page/sign-in-page.component.ts
+++ src/app/sign-in-page/sign-in-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-sign-in-page',
   standalone: true,
-  imports: [CommonModule, BodyComponent],
+  imports: [CommonModule],
   templateUrl: './sign-in-page.component.html',
   styleUrl: './sign-in-page.component.css',
 })
