80e480f129d364f13ac31f20e2955f064b866b54
alt0828 <alt0828alt0828@gmail.com>
2024-01-09 12:44:55 +0800

    feat(html): 製作your feed 超連結

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { SignInPageComponent } from '../sign-in-page/sign-in-page.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, NavBarComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    NavBarComponent,
+    SignInPageComponent,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
