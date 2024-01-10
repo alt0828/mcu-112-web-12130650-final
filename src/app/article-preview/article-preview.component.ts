a335c2601deb201d7755a5809266520afec48517
alt0828 <alt0828alt0828@gmail.com>
2024-01-10 12:18:50 +0800

    feat(article): 製作文章元件及清單

---
+++ src/app/article-preview/article-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, Host, HostBinding } from '@angular/core';
+import { TagComponent } from '../tag/tag.component';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-article-preview',
+  standalone: true,
+  imports: [NgFor, TagComponent],
+  templateUrl: './article-preview.component.html',
+  styleUrl: './article-preview.component.css',
+})
+export class ArticlePreviewComponent {
+  @HostBinding('class') class = 'article-preview';
+  tags = ['enim', 'repellat', 'est', 'eos'];
+}
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagListComponent } from '../tag-list/tag-list.component';
+import { ArticleListComponent } from '../article-list/article-list.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [TagListComponent],
+  imports: [TagListComponent, ArticleListComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
 })
