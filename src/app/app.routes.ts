6b8580a01dd7baced01f43cc4a970162259a4175
alt0828 <alt0828alt0828@gmail.com>
2024-01-11 00:54:13 +0800

    feat(article-form): 製作新文章表單切換功能

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { ArticleFormPageComponent } from './article-form-page/article-form-page.component';
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NewArticlePageComponent } from './new-article-page/new-article-page.component';
@@ -7,7 +8,7 @@ import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'Home' },
   { path: 'Home', component: HomePageComponent },
-  { path: 'New Article', component: NewArticlePageComponent },
+  { path: 'article/form', component: ArticleFormPageComponent },
   { path: 'Sign in', component: SignInPageComponent },
   { path: 'Sign up', component: SignUpPageComponent },
   { path: 'conduit', component: HomePageComponent },
