6168a5d5f7560f783d7ce144e6c5849ab47409af
alt0828 <alt0828alt0828@gmail.com>
2024-01-11 15:22:16 +0800

    feat(ts): 文章資料類別定義

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -2,15 +2,33 @@ import { FavoriteButtonComponent } from './../favorite-button/favorite-button.co
 import { Component, HostBinding } from '@angular/core';
 
 import { TagListComponent } from '../tag-list/tag-list.component';
+import { Article } from '../article';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [TagListComponent, FavoriteButtonComponent],
+  imports: [DatePipe, TagListComponent, FavoriteButtonComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
   @HostBinding('class') class = 'article-preview';
+
   tags = ['enim', 'repellat', 'est', 'eos'];
+
+  article = new Article({
+    id: 1,
+    title:
+      'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
+    content: `
+      Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui
+      suscipit debitis et et voluptates voluptatem voluptatibus. Quas voluptatum
+      quae corporis corporis possimus.
+    `,
+    favoriteCount: 30,
+    author: 'Albert',
+    createDate: new Date(2024, 6, 4),
+    tags: ['enim', 'repellat', 'est', 'eos'],
+  });
 }
